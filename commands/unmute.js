46f87c122e3540e1d2102a945a8c13cc8d7d6e48
mrx2468555-gif <mrx2468555@gmail.com>
2025-11-04 23:07:43 +0200

    Change unmute message to Arabic

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,6 +1,6 @@
 async function unmuteCommand(sock, chatId) {
     await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
-    await sock.sendMessage(chatId, { text: 'The group has been unmuted.' });
+    await sock.sendMessage(chatId, { text: 'تم إلغاء كتم صوت المجموعة.' });
 }
 
 module.exports = unmuteCommand;
