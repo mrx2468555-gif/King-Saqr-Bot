a62bcf8ce522ef68cd2c5af9f8a63f4ad6c1073a
mrx2468555-gif <mrx2468555@gmail.com>
2025-11-04 22:34:32 +0200

    Change welcome message to Arabic for group command

--- commands/welcome.js
+++ commands/welcome.js
@@ -3,7 +3,7 @@ const { handleWelcome } = require('../lib/welcome');
 async function welcomeCommand(sock, chatId, message, match) {
     // Check if it's a group
     if (!chatId.endsWith('@g.us')) {
-        await sock.sendMessage(chatId, { text: 'This command can only be used in groups.' });
+        await sock.sendMessage(chatId, { text: 'لا يمكن استخدام هذا الأمر إلا في المجموعات.' });
         return;
     }
 
